4e736f274c86ed560b2515992cc8682a001a69d0
ArunJeganathan1909 <jeganathanarun1105@gmail.com>
2024-02-06 20:02:31 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
 
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}!!`);
 });
-jgfgfghfghh
