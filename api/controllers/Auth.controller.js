19c58b5c5c9fc62d3b8f5c58dbf570049fe349b2
ArunJeganathan1909 <jeganathanarun1105@gmail.com>
2024-02-07 14:34:39 +0530

    create signin api route

--- api/controllers/Auth.controller.js
+++ api/controllers/Auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 const bcryptSalt = 10;
 
 const signup = async (req, res, next) => {
@@ -31,4 +32,34 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid PassWord"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("acces-token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
--- api/routes/Auth.route.js
+++ api/routes/Auth.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const signupController = require("../controllers/Auth.controller");
-
+const { signup, signin } = require("../controllers/Auth.controller");
 const router = express.Router();
 
-router.post("/signup", signupController);
+router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
