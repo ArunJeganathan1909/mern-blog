e5c64e30e8cbd234b80d87d7b0e8748b64ed1756
ArunJeganathan1909 <jeganathanarun1105@gmail.com>
2024-02-08 02:10:11 +0530

    add Google Oauth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjVloKfIICAkLiG4HSbpPPZioOhSeVjsE",
+  authDomain: "mern-blog-f780b.firebaseapp.com",
+  projectId: "mern-blog-f780b",
+  storageBucket: "mern-blog-f780b.appspot.com",
+  messagingSenderId: "641503550749",
+  appId: "1:641503550749:web:126d159dc363b74c4415f4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import {
   signInStart,
   signInFailure,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
@@ -97,6 +98,7 @@ const Signin = () => {
                 "Sign in"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't Have an account?</span>
