9ab9b3cdf6bce55ed96913cadb62de396ed2b57a
ArunJeganathan1909 <jeganathanarun1105@gmail.com>
2024-02-07 14:47:23 +0530

    complete sign in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
